e16997bc4615385a994d39a5e403111f20d7e8d1
kdingman <kcdonlan@yahoo.com>
2022-03-08 16:06:43 -0500

    initial set up, created files, created the db, created the .gitignore, created the server.js, created the .env file

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS auditorium_db;
+
+CREATE DATABASE auditorium_db;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+// will need to add const routes here
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.store);
+
+const sess = {
+    secret: 'Need to put our secret here',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+// Template Engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Will need to connect our Routes Here
+
+// Turn on Connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
