e1706d16fea2ece0b69d3bcae7215bb1b8305bff
kdingman <kcdonlan@yahoo.com>
2022-03-09 20:56:22 -0500

    issue #6 created the user, profile, post, comment and index models for the database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,50 @@
 const User = require('./User');
+const Profile = require('./Profile');
+const Post =  require('./Post');
+const Comment = require('./Comment');
+
+// Create Associations between Models
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Profile.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Profile.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Profile.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Profile, Post, Comment };
 
 
 
 
 
-module.exports = { User };
+
+module.exports = { User, Profile, Post, Comment };
