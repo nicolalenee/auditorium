c1a430148b363b312eb0e5c41a071fbd72e7f182
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-15 19:50:23 -0400

    routes

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+router.get('/', (req, res) => {
+  res.render('homepage')
+});
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const profileRoutes = require('./profile-routes');
 const commentRoutes = require('./comment-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
 router.use('/profile', profileRoutes);
 router.use('/comment', commentRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
