626b7503378bccebac671d3ac2eca4a83e0912d8
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-15 23:54:48 -0400

    render homepage and login routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,27 @@
+const { User } = require('../models');
+const { route } = require('./api');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.render('homepage');
+});
+
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if(req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
 });
 
 module.exports = router;
