2de557171a6336aa7ca115462902aac67da0576c
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-16 09:08:24 -0400

    create settings page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,15 @@
+const res = require('express/lib/response');
 const { User } = require('../models');
 const { route } = require('./api');
 
 const router = require('express').Router();
 
+// render the homepage
 router.get('/', (req, res) => {
   res.render('homepage');
 });
 
-
+// render the login page
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -16,12 +18,23 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// render the signup page
 router.get('/signup', (req, res) => {
   if(req.session.loggedIn) {
     res.redirect('/');
     return;
   }
   res.render('signup');
+});
+
+// render the new post page
+router.get('/new-post', (req, res) => {
+  res.render('new-post');
+})
+
+// render the settings page
+router.get('/settings', (req, res)=> {
+  res.render('settings');
 });
 
 module.exports = router;
