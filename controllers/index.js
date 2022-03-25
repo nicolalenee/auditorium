14528b20e2b3418ac92edeee6b15926445f448f4
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-25 17:39:17 -0400

    added post routes router to the controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/user', userRoutes);
+router.use('/post', postRoutes);
 module.exports = router;
