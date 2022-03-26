75ab48510544f5fcf32f0232039c969ef1c085d8
kdingman <kcdonlan@yahoo.com>
2022-03-26 09:50:37 -0400

    profile seeds were not importing data, fixed issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.SESSION_SECRET,
+    secret: 'secrect secret session',
     cookie: {},
     resave: false,
     saveUninitialized: true,
